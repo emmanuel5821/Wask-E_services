1ff9c612f3b0851387a9c2763cb54fadb3860f34
emmanuel5821 <eob72337@udelasalle.edu.mx>
2022-10-11 15:14:34 -0500

    Emmanuel-Primera parte actividad Drawer Navigation

---
+++ app/src/main/java/mx/salle/proyectofinal/UsuarioComun.kt
@@ -0,0 +1,10 @@
+package mx.salle.proyectofinal
+
+class UsuarioComun {
+    var ususarioId = 0
+    var nombreUsuario = ""
+    var apellidosUario = ""
+    var domicilioUsuario = ""
+    var telefonoUsuario = ""
+
+}
---
+++ app/src/main/java/mx/salle/proyectofinal/Viajes.kt
@@ -0,0 +1,4 @@
+package mx.salle.proyectofinal
+
+class Viajes {
+}
